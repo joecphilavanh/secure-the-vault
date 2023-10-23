0372e81eff83aef70589f9dc4670a6250023bfd1
Joe Philavanh <joecphilavanh@DESKTOP-PL0LMS4.localdomain>
2023-10-23 12:30:38 -0700

    my secure the vault exercise

---
+++ secureTheVault.js
@@ -0,0 +1,10 @@
+// create a string for the user 
+const message = `You have recieved this message because you have been chosen to open an important vault.Here is the secret combination`;
+//Assign three variables using arithmitic 
+const code1 = 10%11;
+const code2 = 10*4;
+const code3 = 40-1;
+const combination = ` ${code1} - ${code2} - ${code3}`;
+//create a dialog box displaying the code. 
+alert(message + combination );
+console.log(message + combination );
